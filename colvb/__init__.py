144ea7c363dbf1b977d0b486539a53223cff0e4e
James Hensman <james.hensman@gmail.com>
2013-01-04 10:13:37 +0000

    added the __init__.py file for the colvb module

---
+++ colvb/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2012 James Hensman
+# Licensed under the GPL v3 (see LICENSE.txt)
+
+from MOG import MOG
+from MMOG import MMOG
+from MOHGP import MOHGP
+import bitseq
+import utilities
