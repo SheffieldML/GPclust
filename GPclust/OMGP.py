6ee9dd1b4135a3a1924e8d82ebd1263bfcf88586
Valentine Svensson <vale@ebi.ac.uk>
2015-07-29 11:50:59 +0100

    Predictive means for the OMGP

--- GPclust/OMGP.py
+++ GPclust/OMGP.py
@@ -10,20 +10,20 @@ from utilities import multiple_mahalanobis
 class OMGP(CollapsedMixture):
     """ OMGP Model
     """
-    def __init__(self, X, Y, K=2, alpha=1., prior_Z='symmetric', name='OMGP'):
+    def __init__(self, X, Y, K=2, kernels=None, alpha=1., prior_Z='symmetric', name='OMGP'):
 
         N, self.D = Y.shape
         self.Y = Y
         self.X = X
 
-        self.kern = GPy.kern.RBF(input_dim=1) ** GPy.kern.Coregionalize(1, output_dim=K)
+        self.s2 = 1.
+
+        if kernels == None:
+            self.kern = []
+            for i in range(K):
+                self.kern.append(GPy.kern.RBF(input_dim=1))
 
         CollapsedMixture.__init__(self, N, K, prior_Z, alpha, name)
-
-        # Initialize kernel
-        self.Xtot = np.hstack((self.X, np.atleast_2d(np.argmax(self.phi, 1)).T))
-        self.Sy = self.kern.K(self.Xtot)
-        # self.Sy_inv, self.Sy_chol, self.Sy_chol_inv, self.Sy_logdet = pdinv(self.Sy + np.eye(self.D) * 1e-6)
 
         # self.do_computations()
 
@@ -32,22 +32,7 @@ class OMGP(CollapsedMixture):
         Here we do all the computations that are required whenever the kernels
         or the variational parameters are changed.
         """
-        # sufficient stats.
-        self.ybark = np.dot(self.phi.T,self.Y).T
-
-        # compute posterior variances of each cluster (lambda_inv)
-        tmp = backsub_both_sides(self.Sy_chol, self.Sf, transpose='right')
-        self.Cs = [np.eye(self.D) + tmp * phi_hat_i for phi_hat_i in self.phi_hat]
-
-        self._C_chols = [jitchol(C) for C in self.Cs]
-        self.log_det_diff = np.array([2.*np.sum(np.log(np.diag(L))) for L in self._C_chols])
-        tmp = [dtrtrs(L, self.Sy_chol.T, lower=1)[0] for L in self._C_chols]
-        self.Lambda_inv = np.array([( self.Sy - np.dot(tmp_i.T, tmp_i) )/phi_hat_i if (phi_hat_i>1e-6) else self.Sf for phi_hat_i, tmp_i in zip(self.phi_hat, tmp)])
-
-        #posterior mean and other useful quantities
-        self.Syi_ybark, _ = dpotrs(self.Sy_chol, self.ybark, lower=1)
-        self.Syi_ybarkybarkT_Syi = self.Syi_ybark.T[:,None,:]*self.Syi_ybark.T[:,:,None]
-        self.muk = (self.Lambda_inv*self.Syi_ybark.T[:,:,None]).sum(1).T
+        pass
 
     def bound(self):
         """
@@ -62,3 +47,26 @@ class OMGP(CollapsedMixture):
 
     def predict_components(self, Xnew):
         """The predictive density under each component"""
+        mus = []
+        for i, kern in enumerate(self.kern):
+            K = kern.K(self.X)
+            kx = kern.K(self.X, Xnew)
+
+            # This Cholesky version isn't working... Fix later
+            # I = np.eye(self.N)
+            # B12 = np.sqrt(np.diag(self.phi[:, i] / self.s2))
+            # R = jitchol(I + B12.dot(K.dot(B12)))
+            # B12y = B12.dot(self.Y)
+            # tmp1 = np.linalg.solve(R.T, B12y)
+            # tmp2 = np.linalg.solve(R, tmp1)
+            # mu = kx.T.dot(B12.dot(tmp2))
+
+            # Don't do this due to numerical stability!
+            B_inv = np.diag(1. / (self.phi[:, i] / self.s2))
+            self.K = K
+            self.B_inv = B_inv
+            mu = kx.T.dot(np.linalg.solve((K + B_inv), self.Y))
+
+            mus.append(mu)
+
+        return np.array(mus)
