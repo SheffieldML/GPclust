Simplified calculation of individual GP log likelihoods. Though not it seems numerically unstable, but it should at least be correct

GPclust/MOHGP.py
@@ -28,7 +28,7 @@ class MOHGP(CollapsedMixture):
     """
     def __init__(self, X, kernF, kernY, Y, K=2, alpha=1., prior_Z='symmetric', name='MOHGP'):
 
-        N,self.D = Y.shape
+        N, self.D = Y.shape
         self.Y = Y
         self.X = X
         assert X.shape[0]==self.D, "input data don't match observations"
@@ -110,7 +110,7 @@ class MOHGP(CollapsedMixture):
         Byks = [np.dot(Bi,yk) for Bi,yk in zip(B_invs,self.ybark.T)]
         tmp = sum([np.dot(Byk[:,None],Byk[None,:])/np.power(ph_k,3)\
                 -Syi_ybarkybarkT_Syi/ph_k -Bi/ph_k for Bi, Byk, yyT, ph_k, Syi_ybarkybarkT_Syi in zip(B_invs, Byks, ybarkybarkT, self.phi_hat, self.Syi_ybarkybarkT_Syi) if ph_k >1e-6])
-        tmp += (self.K-self.N)*self.Sy_inv
+        tmp += (self.K - self.N) * self.Sy_inv
         tmp += mdot(self.Sy_inv,self.YTY,self.Sy_inv)
         tmp /= 2.
 
@@ -122,13 +122,13 @@ class MOHGP(CollapsedMixture):
         Compute the lower bound on the marginal likelihood (conditioned on the
         GP hyper parameters).
         """
-        return -0.5*(self.N*self.D*np.log(2.*np.pi)\
-                + self.log_det_diff.sum()\
-                + self.N*self.Sy_logdet\
-                + np.sum(self.YTY*self.Sy_inv))\
-                + 0.5*np.sum(self.Syi_ybarkybarkT_Syi*self.Lambda_inv)\
-                + self.mixing_prop_bound()\
-                + self.H
+        return -0.5 * ( self.N * self.D * np.log(2. * np.pi) \
+                        + self.log_det_diff.sum() \
+                        + self.N * self.Sy_logdet \
+                        + np.sum(self.YTY * self.Sy_inv) ) \
+               + 0.5 * np.sum(self.Syi_ybarkybarkT_Syi * self.Lambda_inv) \
+               + self.mixing_prop_bound() \
+               + self.H
 
     def vb_grad_natgrad(self):
         """

GPclust/OMGP.py
@@ -66,11 +66,18 @@ class OMGP(CollapsedMixture):
 
         for i, kern in enumerate(self.kern):
             K = kern.K(self.X)
-            B12 = np.sqrt(np.diag(self.phi[:, i] / self.s2))
-            I = np.eye(self.N)
-            R = jitchol(I + B12.dot(K.dot(B12))).T
-            GP_bound += -0.5 * np.linalg.norm(np.linalg.solve(R, B12.dot(self.Y))) ** 2
-            GP_bound += -self.D * np.trace(np.log(R))
+            B_inv = np.diag(1. / (self.phi[:, i] / self.s2))
+
+            # Data fit, numerically unstable?
+            alpha = np.linalg.solve(K + B_inv, self.Y)
+            GP_bound += -0.5 * np.dot(self.Y.T, alpha)
+
+            # Penalty
+            GP_bound += -0.5 * np.linalg.slogdet(K + B_inv)[1]
+
+            # Constant
+            GP_bound += self.N / 2.0 * np.log(2. * np.pi)
+
 
         mixing_bound = self.mixing_prop_bound() + self.H
         norm_bound = -0.5 * self.D * (self.phi * np.log(2 * np.pi * self.s2)).sum()
@@ -87,15 +94,21 @@ class OMGP(CollapsedMixture):
             K = kern.K(self.X)
             I = np.eye(self.N)
             B12 = np.sqrt(np.diag(self.phi[:, i] / self.s2))
-            R = jitchol(I + B12.dot(K.dot(B12))).T
+
+            # R = jitchol(I + B12.dot(K.dot(B12))).T
+
+            B_inv = np.diag(1. / (self.phi[:, i] / self.s2))            
+            R = jitchol((K + B_inv)).T
+
             muk = np.atleast_2d(self.predict(self.X, i))
             ynmk2[:, i:(i + 1)] = multiple_mahalanobis(self.Y, muk.T, R)
 
         grad_phi = (self.mixing_prop_bound_grad() - 
                      0.0) + \
                    (self.Hgrad - 0.5 * ynmk2)
-        natgrad = grad_phi - np.sum(self.phi*grad_phi,1)[:,None]
-        grad = natgrad*self.phi
+
+        natgrad = grad_phi - np.sum(self.phi * grad_phi, 1)[:, None]
+        grad = natgrad * self.phi
 
         return grad.flatten(), natgrad.flatten()
 