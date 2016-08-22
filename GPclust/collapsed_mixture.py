f99635c846e1017e45cf96c15817c72d1a370f97
mathDR <dan.marthaler@gmail.com>
2016-08-22 10:44:37 -0700

    changed variable names in collapsed_mixture for readability

--- GPclust/collapsed_mixture.py
+++ GPclust/collapsed_mixture.py
@@ -4,13 +4,13 @@
 import numpy as np
 
 try:
-    from .utilities import ln_dirichlet_C, softmax_weave
+    from utilities import ln_dirichlet_C, softmax_weave
 except ImportError:
-    from .np_utilities import ln_dirichlet_C
-    from .np_utilities import softmax_numpy as softmax_weave
+    from np_utilities import ln_dirichlet_C
+    from np_utilities import softmax_numpy as softmax_weave
 
 from scipy.special import gammaln, digamma
-from .collapsed_vb import CollapsedVB
+from collapsed_vb import CollapsedVB
 
 class CollapsedMixture(CollapsedVB):
     """
@@ -21,24 +21,24 @@ class CollapsedMixture(CollapsedVB):
     This handles the mixing proportion part of the model,
     as well as providing generic functions for a merge-split approach
     """
-    def __init__(self, N, K, prior_Z='symmetric', alpha=1.0, name='col_mix'):
+    def __init__(self, num_data, num_clusters, prior_Z='symmetric', alpha=1.0, name='col_mix'):
         """
         Arguments
         =========
-        N: the number of data
-        K: the (initial) number of cluster (or truncation)
+        num_data: the number of data
+        num_clusters: the (initial) number of cluster (or truncation)
         prior_Z  - either 'symmetric' or 'DP', specifies whether to use a symmetric Dirichlet prior for the clusters, or a (truncated) Dirichlet Process.
         alpha: parameter of the Dirichelt (process)
 
         """
         CollapsedVB.__init__(self, name)
-        self.N, self.K = N, K
+        self.num_data, self.num_clusters = num_data, num_clusters
         assert prior_Z in ['symmetric','DP']
         self.prior_Z = prior_Z
         self.alpha = alpha
 
         #random initial conditions for the vb parameters
-        self.phi_ = np.random.randn(self.N, self.K)
+        self.phi_ = np.random.randn(self.num_data, self.num_clusters)
         self.phi, logphi, self.H = softmax_weave(self.phi_)
         self.phi_hat = self.phi.sum(0)
         self.Hgrad = -logphi
@@ -50,7 +50,7 @@ class CollapsedMixture(CollapsedVB):
         """
         Accept a vector representing the variatinoal parameters, and reshape it into self.phi
         """
-        self.phi_ = phi_.reshape(self.N, self.K)
+        self.phi_ = phi_.reshape(self.num_data, self.num_clusters)
         self.phi, logphi, self.H = softmax_weave(self.phi_)
         self.phi_hat = self.phi.sum(0)
         self.Hgrad = -logphi
@@ -68,12 +68,12 @@ class CollapsedMixture(CollapsedVB):
         The portion of the bound which is provided by the mixing proportions
         """
         if self.prior_Z=='symmetric':
-            return ln_dirichlet_C(np.ones(self.K)*self.alpha) -ln_dirichlet_C(self.alpha + self.phi_hat)
+            return ln_dirichlet_C(np.ones(self.num_clusters)*self.alpha) -ln_dirichlet_C(self.alpha + self.phi_hat)
         elif self.prior_Z=='DP':
             A = gammaln(1. + self.phi_hat)
             B = gammaln(self.alpha + self.phi_tilde)
             C = gammaln(self.alpha + 1. + self.phi_tilde_plus_hat)
-            D = self.K*(gammaln(1.+self.alpha) - gammaln(self.alpha))
+            D = self.num_clusters*(gammaln(1.+self.alpha) - gammaln(self.alpha))
             return A.sum() + B.sum() - C.sum() + D
         else:
             raise NotImplementedError("invalid mixing proportion prior type: %s" % self.prior_Z)
@@ -106,7 +106,7 @@ class CollapsedMixture(CollapsedVB):
         """Remove any cluster which has no data assigned to it"""
         i = self.phi_hat>threshold
         phi_ = self.phi_[:,i]
-        self.K = i.sum()
+        self.num_clusters = i.sum()
         self.set_vb_param(phi_)
 
     def try_split(self, indexK=None, threshold=0.9, verbose=True, maxiter=100, optimize_params=None):
@@ -126,8 +126,8 @@ class CollapsedMixture(CollapsedVB):
 
         """
         if indexK is None:
-            indexK = np.random.multinomial(1,self.phi_hat/self.N).argmax()
-        if indexK > (self.K-1):
+            indexK = np.random.multinomial(1,self.phi_hat/self.num_data).argmax()
+        if indexK > (self.num_clusters-1):
             return False #index exceed no. clusters
         elif self.phi_hat[indexK]<1:
             return False # no data to split
@@ -142,10 +142,10 @@ class CollapsedMixture(CollapsedVB):
         phi_old = self.get_vb_param().copy()
         self._optimizer_copy_transformed = False  # Redo transform in case parameters have been unlinked
         param_old = self.optimizer_array.copy()
-        old_K = self.K
+        old_num_clusters = self.num_clusters
 
         #re-initalize
-        self.K += 1
+        self.num_clusters += 1
         self.phi_ = np.hstack((self.phi_,self.phi_.min(1)[:,None]))
         indexN = np.nonzero(self.phi[:,indexK] > threshold)[0]
 
@@ -169,15 +169,15 @@ class CollapsedMixture(CollapsedVB):
 
         bound_increase = bound_new-bound_old
         if (bound_increase < 1e-3):
-            self.K = old_K
+            self.num_clusters = old_num_clusters
             self.set_vb_param(phi_old)
             self.optimizer_array = param_old
-            if verbose:print("split failed, bound changed by: ",bound_increase, '(K=%s)' % self.K)
+            if verbose:print("split failed, bound changed by: ",bound_increase, '(K=%s)' % self.num_clusters)
 
             return False
 
         else:
-            if verbose:print("split suceeded, bound changed by: ", bound_increase, ',', self.K-old_K,' new clusters', '(K=%s)' % self.K)
+            if verbose:print("split suceeded, bound changed by: ", bound_increase, ',', self.num_clusters-old_num_clusters,' new clusters', '(K=%s)' % self.num_clusters)
             if verbose:print("optimizing new split to convergence:")
             if optimize_params:
                 self.optimize(**optimize_params)
@@ -191,7 +191,7 @@ class CollapsedMixture(CollapsedVB):
         """
         perform recursive splits on each of the existing clusters
         """
-        for kk in range(self.K):
+        for kk in range(self.num_clusters):
             self.recursive_splits(kk, verbose=verbose)
 
     def recursive_splits(self,k=0, verbose=True, optimize_params=None):
@@ -200,7 +200,7 @@ class CollapsedMixture(CollapsedVB):
         """
         success = self.try_split(k, verbose=verbose, optimize_params=optimize_params)
         if success:
-            if not k==(self.K-1):
-                self.recursive_splits(self.K-1, verbose=verbose, optimize_params=optimize_params)
+            if not k==(self.num_clusters-1):
+                self.recursive_splits(self.num_clusters-1, verbose=verbose, optimize_params=optimize_params)
             self.recursive_splits(k, verbose=verbose, optimize_params=optimize_params)
 
